791f95a42af3abc553ad9599a6f8dbec0d45c263
Antonio Berbel <aberbel@gmail.com>
2026-03-18 20:56:27 +0100

    Prueba cambiando en pre

--- src/main/java/ramas/Prueba.java
+++ src/main/java/ramas/Prueba.java
@@ -1,9 +1,12 @@
 package ramas;
 
+import java.time.Instant;
+
 public class Prueba {
 
 	public static void main(String[] args) {
 		System.out.println("prueba");
+		Instant fecha = Instant.now();
 	}
 
 }
